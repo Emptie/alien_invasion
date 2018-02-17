fad633eaa65a9d392d988ea168460658052bd72f
Emptie <emptielol@gmail.com>
2018-02-17 14:19:58 +0800

    创建并移动外星人群

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,26 +4,31 @@ from pygame.sprite import Group
 
 from setting import Setting
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    ai_setting = Setting()
-    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
+    ai_settings = Setting()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一艘飞船
-    ship = Ship(ai_setting, screen)
-    # 创建一个用于存储子弹的编组
+    # 创建一艘飞船,一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.creat_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
 
-        gf.check_events(ai_setting, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_setting, screen, ship, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
